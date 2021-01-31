3139ff2ef52cd434537e444dde87c204fca6e2ad
yuchiki <yuki08281993@gmail.com>
2021-01-31 20:07:00 +0900

    refactor test

--- cmd/atcoderHelper/cmd/version_test.go
+++ cmd/atcoderHelper/cmd/version_test.go
@@ -2,25 +2,60 @@ package cmd
 
 import (
 	"bytes"
+	"errors"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/spf13/cobra"
 )
 
 func TestVersion_Execute(t *testing.T) {
-	var buf bytes.Buffer
-
 	cmd := newVersionCmd()
-	cmd.SetOut(&buf)
-	err := cmd.Execute()
-	if err != nil {
-		t.Error(err)
-	}
+	checkCommand(
+		t,
+		cmd,
+		outputShouldBe(`{"Version":"given by LDFLAGS","Commit":"given by LDFLAGS","Edited":"given by LDFLAGS","Date":"given by LDFLAGS"}`+"\n"),
+		shouldNotHaveError(),
+	)
+}
 
-	actual := buf.String()
+type OutputCheck func(t *testing.T, output string)
+type ErrorCheck func(t *testing.T, err error)
 
-	expected := `{"Version":"given by LDFLAGS","Commit":"given by LDFLAGS","Edited":"given by LDFLAGS","Date":"given by LDFLAGS"}` + "\n"
-	if diff := cmp.Diff(actual, expected); diff != "" {
-		t.Error(diff)
+func outputShouldBe(expected string) OutputCheck {
+	return func(t *testing.T, output string) {
+		t.Helper()
+		if diff := cmp.Diff(output, expected); diff != "" {
+			t.Error(diff)
+		}
 	}
+}
+
+func errorShouldBe(expected error) ErrorCheck {
+	return func(t *testing.T, err error) {
+		t.Helper()
+		if !errors.Is(expected, err) {
+			t.Errorf("expected error %v, but actual error %v", expected, err)
+		}
+	}
+}
+
+func shouldNotHaveError() ErrorCheck {
+	return func(t *testing.T, err error) {
+		t.Helper()
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func checkCommand(t *testing.T, command *cobra.Command, outputCheck OutputCheck, errorCheck ErrorCheck) {
+	t.Helper()
+	var buf bytes.Buffer
+	command.SetOut(&buf)
+	err := command.Execute()
+	errorCheck(t, err)
+
+	output := buf.String()
+	outputCheck(t, output)
 }
