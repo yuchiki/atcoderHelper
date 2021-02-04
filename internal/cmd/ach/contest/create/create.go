302f96b19ccf723e70bd5a050ff3141179fb0e38
yuchiki <yuki08281993@gmail.com>
2021-02-05 03:22:46 +0900

    fix

--- internal/cmd/ach/contest/create/create.go
+++ internal/cmd/ach/contest/create/create.go
@@ -2,15 +2,18 @@ package create
 
 import (
 	"os/exec"
+	"os/user"
 	"path"
 
 	"github.com/spf13/cobra"
 )
 
 func NewContestCreateCmd() *cobra.Command {
+	user, _ := user.Current()
+	templateDirName := path.Join(user.HomeDir, "projects", "private", "atcoder", "D")
 	taskNames := []string{"A", "B", "C", "D", "E", "F"}
 
-	templateDir := new(string)
+	useDefaultTemplate := new(bool)
 
 	cmd := &cobra.Command{
 		Use:   "create",
@@ -19,19 +22,18 @@ func NewContestCreateCmd() *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			contestName := args[0]
-
 			exec.Command("mkdir", contestName).Run()
 			for _, taskName := range taskNames {
 				taskDirName := path.Join(contestName, taskName)
-				exec.Command("cp", "-r", *templateDir, taskDirName).Run()
+				exec.Command("cp", "-r", templateDirName, taskDirName).Run()
 			}
 
 			return nil
 		},
 	}
 
-	cmd.Flags().StringVarP(templateDir, "templateDir", "t", "", "contest template")
-	cmd.MarkFlagRequired("templateDir")
+	cmd.Flags().BoolVarP(useDefaultTemplate, "default-template", "d", false, "use default contest template")
+	cmd.MarkFlagRequired("default-template")
 
 	return cmd
 }
