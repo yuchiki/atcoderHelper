e414e4f725b937826e989f805102243bdf31097e
yuchiki <yuki08281993@gmail.com>
2021-02-12 19:32:16 +0900

    ach

--- internal/cmd/ach/ach.go
+++ internal/cmd/ach/ach.go
@@ -1,10 +1,22 @@
 package ach
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/yuchiki/atcoderHelper/internal/cmd/ach/contest"
 	"github.com/yuchiki/atcoderHelper/internal/cmd/ach/test"
 	"github.com/yuchiki/atcoderHelper/internal/cmd/ach/version"
+	"github.com/yuchiki/atcoderHelper/internal/config"
+)
+
+var (
+	cfgFile     string
+	taskCfgFile string
 )
 
 // NewAchCmd returns ach command.
@@ -14,6 +26,9 @@ func NewAchCmd() *cobra.Command {
 		Short: "ach automates routine work you does when you participate AtCoder contests",
 		Long:  `ach automates routine work you does when you participate AtCoder contests. `,
 	}
+
+	cmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ach/config.yaml)")
+	cmd.PersistentFlags().StringVar(&taskCfgFile, "task-config", "", "task config file (default is ./achTaskConfig.yaml")
 
 	registerSubcommands(cmd)
 
@@ -26,11 +41,49 @@ func registerSubcommands(cmd *cobra.Command) {
 	cmd.AddCommand(test.NewTestCmd())
 }
 
-/*
- func init() {
+func init() {
 	cobra.OnInitialize(initConfig)
 }
 
 func initConfig() {
+	readAppConfig()
+	readTaskConfig()
 }
-*/
+
+func readAppConfig() {
+	if cfgFile != "" {
+		viper.SetConfigName(cfgFile)
+		viper.ReadInConfig()
+	} else {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(path.Join(home, ".ach"))
+		viper.SetConfigName("config")
+		err = viper.ReadInConfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	viper.UnmarshalExact(&config.GlobalAppConfig)
+}
+
+func readTaskConfig() {
+	if taskCfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+		viper.ReadInConfig()
+	} else {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("achTaskConfig")
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	viper.UnmarshalExact(&config.GlobalTaskConfig)
+}
