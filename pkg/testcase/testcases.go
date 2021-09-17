483f03121214d599101820c576fe901565d849b9
yuchiki <yuki08281993@gmail.com>
2021-09-17 23:25:33 +0900

    newtestcases

--- pkg/testcase/testcases.go
+++ pkg/testcase/testcases.go
@@ -8,3 +8,28 @@ type Testcases struct {
 type Summary struct {
 	Status Status
 }
+
+func NewTestCases(bareTestcases []Testcase) Testcases {
+	allPass := func() bool {
+		for _, testcase := range bareTestcases {
+			if testcase.Status != Pass {
+				return false
+			}
+		}
+		return true
+	}
+
+	var status Status
+	if allPass() {
+		status = Pass
+	} else {
+		status = NotPassed
+	}
+
+	return Testcases{
+		Testcases: bareTestcases,
+		Summary: Summary{
+			Status: status,
+		},
+	}
+}
