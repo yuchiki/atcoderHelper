680f6e41e38e50d70b364e4caf493a585a0b39dc
yuchiki <yuki08281993@gmail.com>
2021-09-17 22:52:17 +0900

    wut dis

--- internal/repository/testcases.go
+++ internal/repository/testcases.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Testcase struct {
-	Input  string
-	Output string
+	Input    string
+	Expected string
 }
 
 var (
-	inputSampleRegex  = regexp.MustCompile(`入力例 (\d+)`)
-	outputSampleRegex = regexp.MustCompile(`出力例 (\d+)`)
+	inputSampleRegex    = regexp.MustCompile(`入力例 (\d+)`)
+	expectedSampleRegex = regexp.MustCompile(`出力例 (\d+)`)
 )
 
 func FetchTestcases(contest string, task string) ([]Testcase, error) {
@@ -41,26 +41,26 @@ func FetchTestcases(contest string, task string) ([]Testcase, error) {
 		}
 	}
 
-	outputs := []string{}
+	expecteds := []string{}
 	for _, section := range node.GetNodesByTag("section") {
 		title, err := section.GetChildByTag("h3").GetText()
 		if err != nil {
 			break
 		}
 
-		if outputSampleRegex.MatchString(title) {
+		if expectedSampleRegex.MatchString(title) {
 			text, err := section.GetChildByTag("pre").GetText()
 			if err != nil {
 				return nil, err
 			}
 
-			outputs = append(outputs, text)
+			expecteds = append(expecteds, text)
 		}
 	}
 
 	testcases := []Testcase{}
 	for i := 0; i < len(inputs); i++ {
-		testcases = append(testcases, Testcase{inputs[i], outputs[i]})
+		testcases = append(testcases, Testcase{inputs[i], expecteds[i]})
 	}
 
 	return testcases, nil
